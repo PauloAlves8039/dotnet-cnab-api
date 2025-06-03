1f3de8388f6940d3fae09e1de468503ee98b8257
PauloAlves8039 <pj38alves@gmail.com>
2025-06-03 10:36:45 -0300

    feat: implement constructors in dtos

--- src/CNAB.Application/DTOs/StoreDto.cs
+++ src/CNAB.Application/DTOs/StoreDto.cs
@@ -6,4 +6,14 @@ public class StoreDto
     public string Name { get; set; }
     public string OwnerName { get; set; }
     public decimal Balance { get; set; }
+
+    public StoreDto() { }
+
+    public StoreDto(Guid id, string name, string ownerName, decimal balance)
+    {
+        Id = id;
+        Name = name;
+        OwnerName = ownerName;
+        Balance = balance;
+    }
 }
