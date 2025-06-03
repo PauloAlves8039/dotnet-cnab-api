2ebd4c92d152d554a0854138b458fac5701cddce
PauloAlves8039 <pj38alves@gmail.com>
2025-06-03 16:12:12 -0300

    fix: refactor helper class for web api tests

--- tests/CNAB.TestHelpers/Factories/ControllerTestFactory.cs
+++ tests/CNAB.TestHelpers/Factories/ControllerTestFactory.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Moq;
 
-namespace CNAB.WebAPI.Test.Common;
+namespace CNAB.TestHelpers.Factories;
 
 public static class ControllerTestFactory
 {
